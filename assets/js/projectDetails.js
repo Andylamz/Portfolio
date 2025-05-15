3c395362efd312ad1256851949dd77e57629f688
Andy <andy.lamyt@gmail.com>
2025-05-15 02:26:34 +0100

    update: fixed project image display error on mobile devices

--- assets/js/projectDetails.js
+++ assets/js/projectDetails.js
@@ -3,5 +3,12 @@ const project = [
     title: "To-do List",
     tags: ["CSS", "HTML", "JavaScript", "localStorage"],
     src: "./assets/images/projectScreenshots/todoApp.png",
+    github: "https://github.com/Andylamz/TodoList",
+    demo: "https://zesty-cat-660d95.netlify.app/",
+  },
+  {
+    title: "To-do List",
+    tags: ["CSS", "HTML", "JavaScript", "localStorage"],
+    src: "./assets/images/projectScreenshots/todoApp.png",
   },
 ];
