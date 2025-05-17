a675d192ed034bdc9e02ff4626a7e23450c26b76
Andy <andy.lamyt@gmail.com>
2025-05-17 12:38:05 +0100

    update: projects updated

--- assets/js/projectDetails.js
+++ assets/js/projectDetails.js
@@ -1,14 +1,33 @@
 const project = [
   {
     title: "To-do List",
+    category: "Frontend",
     tags: ["CSS", "HTML", "JavaScript", "localStorage"],
     src: "./assets/images/projectScreenshots/todoApp.png",
-    github: "https://github.com/Andylamz/TodoList",
     demo: "https://zesty-cat-660d95.netlify.app/",
+    github: "https://github.com/Andylamz/TodoList",
   },
   {
-    title: "To-do List",
-    tags: ["CSS", "HTML", "JavaScript", "localStorage"],
-    src: "./assets/images/projectScreenshots/todoApp.png",
+    title: "Skill Roller",
+    category: "Frontend",
+    tags: ["CSS", "HTML", "JavaScript"],
+    src: "./assets/images/projectScreenshots/skillRoller.png",
+    demo: "https://enchanting-sunflower-8eb77c.netlify.app/",
+    github: "https://github.com/Andylamz/SkillRoller",
+  },
+  {
+    title: "Weather App",
+    category: "Frontend",
+    tags: [
+      "CSS",
+      "HTML",
+      "JavaScript",
+      "Fetch API",
+      "Weather API",
+      "Geolocation",
+    ],
+    src: "./assets/images/projectScreenshots/weatherApp.png",
+    demo: "https://thriving-vacherin-5d2d92.netlify.app/",
+    github: "https://github.com/Andylamz/Weather-App",
   },
 ];
